f88a49520cea7c04a98d2168b27b3a06fd57e458
lizagqq <lisaverevkina2003@gmail.com>
2025-05-20 23:55:03 +0300

    deleted the button delete from the admin page

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -463,10 +463,7 @@ const Profile = ({ user: userFromProps }) => {
                           <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
                           Редактировать
                         </button>
-                        <button onClick={() => handleDelete(pet.id)} className="flex-1 py-2 px-4 bg-red-500 text-white rounded-lg hover:bg-red-600 transition-colors flex items-center justify-center">
-                          <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5-4h4M3 7h18"></path></svg>
-                          Удалить
-                        </button>
+                        
                       </div>
                     </div>
                   ))}
